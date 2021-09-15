d2d5f6901db918c4b744d633f20a56df29d48748
cgchae <othel5808@gmail.com>
2021-09-15 20:58:49 +0900

    Django 프로젝트 기본 폴더 구성 및 Django 접속 허용 확인

--- back-end/config/settings.py
+++ back-end/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#@9iuu9p9o0e3s@vhe8wm_mrjf@5@lhx9zm8of#$$3y)ham*c2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'UTC'
 
